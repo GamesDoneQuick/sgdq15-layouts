cf926959c15be7dcb8a13e098143fe2a3319881c
Alex Van Camp <email@alexvan.camp>
2015-07-05 20:33:49 -0500

    Finally, the basic framework for the stopwatches is functional.
    Still need to add the interface for manually editing a given stopwatch's time, but the underlying logic is now supported in Rieussec.

--- dashboard/stopwatches.js
+++ dashboard/stopwatches.js
@@ -4,15 +4,41 @@
     var $panel = $bundle.filter('.stopwatches');
     var $stopwatches = $panel.find('.stopwatch');
     var $playPause = $panel.find('.stopwatch-ctrls-playpause');
+    var $reset = $panel.find('.stopwatch-ctrls-reset');
+    var $playAll = $panel.find('button[name="playAll"]');
+    var $pauseAll = $panel.find('button[name="pauseAll"]');
+    var $resetAll = $panel.find('button[name="resetAll"]');
 
-    $playPause.on('tap', function(e) {
+    $playPause.on('click', function(e) {
         var $el = $(e.target);
+        var $stopwatch = $el.closest('.stopwatch');
         if ($el.hasClass('fa-play')) {
-            $el.removeClass('fa-play').addClass('fa-pause');
+            nodecg.sendMessage('startTime', $stopwatches.index($stopwatch));
         } else {
-            $el.removeClass('fa-pause').addClass('fa-play');
+            nodecg.sendMessage('pauseTime', $stopwatches.index($stopwatch));
         }
     });
+
+    $reset.click(function(e) {
+        var $el = $(e.target);
+        var $stopwatch = $el.closest('.stopwatch');
+        nodecg.sendMessage('resetTime', $stopwatches.index($stopwatch));
+    });
+
+    nodecg.Replicant('stopwatches')
+        .on('change', function(oldVal, newVal) {
+            newVal.forEach(function(sw, idx) {
+                var $stopwatch = $($stopwatches[idx]);
+                $stopwatch.find('.stopwatch-status-time').html(sw.time);
+
+                var $playPause = $stopwatch.find('.stopwatch-ctrls-playpause');
+                if (sw.state === 'running') {
+                    $playPause.removeClass('fa-play').addClass('fa-pause');
+                } else {
+                    $playPause.removeClass('fa-pause').addClass('fa-play');
+                }
+            });
+        });
 
     var currentRun = nodecg.Replicant('currentRun')
         .on('change', function(oldVal, newVal) {
@@ -24,4 +50,16 @@
                 $stopwatch.find('.stopwatch-status-runner').html(runner);
             });
         });
+
+    $playAll.click(function() {
+        nodecg.sendMessage('startTime', 'all');
+    });
+
+    $pauseAll.click(function() {
+        nodecg.sendMessage('pauseTime', 'all');
+    });
+
+    $resetAll.click(function() {
+        nodecg.sendMessage('resetTime', 'all');
+    });
 })();
--- extension/stopwatches.js
+++ extension/stopwatches.js
@@ -1,46 +1,76 @@
 'use strict';
 
-var moment = require('moment');
 var Rieussec = require('rieussec');
+var NUM_STOPWATCHES = 4;
 
 module.exports = function (nodecg) {
-    var timesDefault = ['00:00:00', '00:00:00', '00:00:00', '00:00:00'];
-    var times = nodecg.Replicant('times', {defaultValue: timesDefault});
+    var defaultStopwatch = {time: '00:00:00', state: 'stopped'};
+    var defaultStopwatches = [defaultStopwatch, defaultStopwatch, defaultStopwatch, defaultStopwatch];
+    var stopwatches = nodecg.Replicant('stopwatches', {defaultValue: defaultStopwatches});
 
-    // Make an array of 4 Rieussec stopwatches
-    var stopwatches = [null, null, null, null].map(function(val, index) {
+    // Make an array of 4 Rieussec rieussecs
+    var rieussecs = [null, null, null, null].map(function(val, index) {
         // Load the existing time and start the stopwatch at that.
-        var ts = times.value[index].split(':');
-        var ms = Date.UTC(1970, 0, 1, ts[0], ts[1], ts[2]);
-        var stopwatch = new Rieussec();
-        stopwatch.setMilliseconds(ms);
-        stopwatch.on('tick', function(ms) {
-            times.value[index] = moment(ms, 'hh:mm:ss');
+        var startMs = 0;
+        if (stopwatches.value[index].time) {
+            var ts = stopwatches.value[index].time.split(':');
+            startMs = Date.UTC(1970, 0, 1, ts[0], ts[1], ts[2]);
+        }
+
+        var rieussec = new Rieussec();
+        rieussec.setMilliseconds(startMs);
+
+        rieussec.on('tick', function(ms) {
+            stopwatches.value[index].time = msToTime(ms);
         });
-        return stopwatch;
+
+        rieussec.on('state', function(state) {
+            stopwatches.value[index].state = state;
+        });
+
+        return rieussec;
     });
 
     nodecg.listenFor('startTime', function(index) {
         if (index === 'all') {
-            stopwatches.forEach(function(sw) { sw.start(); });
+            rieussecs.forEach(function(sw) { sw.start(); });
+        } else if (index >= 0 && index < NUM_STOPWATCHES) {
+            rieussecs[index].start();
         } else {
-            stopwatches[index].start();
+            nodecg.log.error('index "%d" sent to "startTime" is out of bounds', index);
         }
     });
 
     nodecg.listenFor('pauseTime', function(index) {
         if (index === 'all') {
-            stopwatches.forEach(function(sw) { sw.pause(); });
+            rieussecs.forEach(function(sw) { sw.pause(); });
+        } else if (index >= 0 && index < NUM_STOPWATCHES) {
+            rieussecs[index].pause();
         } else {
-            stopwatches[index].pause();
+            nodecg.log.error('index "%d" sent to "pauseTime" is out of bounds', index);
         }
     });
 
     nodecg.listenFor('resetTime', function(index) {
         if (index === 'all') {
-            stopwatches.forEach(function(sw) { sw.reset(); });
+            rieussecs.forEach(function(sw) { sw.reset(); });
+        } else if (index >= 0 && index < NUM_STOPWATCHES) {
+            rieussecs[index].reset();
         } else {
-            stopwatches[index].reset();
+            nodecg.log.error('index "%d" sent to "resetTime" is out of bounds', index);
         }
     });
+
+    function msToTime(duration) {
+        var milliseconds = parseInt((duration%1000)/100),
+            seconds = parseInt((duration/1000)%60),
+            minutes = parseInt((duration/(1000*60))%60),
+            hours = parseInt((duration/(1000*60*60))%24);
+
+        hours = (hours < 10) ? '0' + hours : hours;
+        minutes = (minutes < 10) ? '0' + minutes : minutes;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+        return hours + ':' + minutes + ':' + seconds;
+    }
 };
